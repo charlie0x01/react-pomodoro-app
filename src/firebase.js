91a63c053b9cd50e76ac54b14404cf5465dee107
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-04-11 03:58:27 +0500

    🔧 config: added .env file for firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+require("dotenv").config("../.env");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCHTsswt1NvEukW_NDwQD3jf1RnCcOZ2T4",
-  authDomain: "mr-pomodoro.firebaseapp.com",
-  databaseURL: "https://mr-pomodoro-default-rtdb.firebaseio.com",
-  projectId: "mr-pomodoro",
-  storageBucket: "mr-pomodoro.appspot.com",
-  messagingSenderId: "696151619311",
-  appId: "1:696151619311:web:3b2147648cf52651da8d1a",
-  measurementId: "G-QMMXPMYBW1"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+export const db = getDatabase(app);
 export default app;
